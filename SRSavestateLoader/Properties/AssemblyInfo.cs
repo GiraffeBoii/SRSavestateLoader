fc9c49ea637c3dd1aac2284e09450c49b4d92237
claytonsmith1027 <claytonsmith1027@tamu.edu>
2026-03-21 20:20:42 -0500

    update mod info and version

--- SRSavestateLoader/Properties/AssemblyInfo.cs
+++ SRSavestateLoader/Properties/AssemblyInfo.cs
@@ -6,8 +6,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("A UMF Mod(umodframework.com) for Slime Rancher")]
 [assembly: AssemblyCompany("My Name @ mywebsite.com")] //Set this to your name/nickname and/or website
 [assembly: AssemblyCopyright("© 2019 My Name. All rights reserved.")] //Set this to your copyright name.
-[assembly: AssemblyVersion("1.0")] //Set this to the version of your mod.
-[assembly: AssemblyFileVersion("1.0")] //Set this to the version of your mod.
+[assembly: AssemblyVersion("1.1")] //Set this to the version of your mod.
+[assembly: AssemblyFileVersion("1.1")] //Set this to the version of your mod.
 
 [assembly: ComVisible(false)]
 [assembly: Guid("5e7e31e4-6bfb-401e-88be-4e347c58fc97")]
